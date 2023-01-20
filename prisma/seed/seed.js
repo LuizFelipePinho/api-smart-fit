beb175f6b806285169d4fcab9dd731b28f709004
LuizFelipePinho <luizfelipesilva458@gmail.com>
2023-01-20 15:34:50 -0300

    add format files

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -2,25 +2,33 @@ import { PrismaClient, Prisma } from "@prisma/client";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 const prisma = new PrismaClient();
-const tb_address = require("./tb_address.json")
-const tb_schedules = require("./tb_schedules.json")
-const tb_situation = require("./tb_situation.json")
-const tb_unit = require("./tb_unit.json")
-const tb_unit_schedules = require("./tb_unit_shedules.json")
+const tb_address_data = require("./tb_address.json");
+const tb_schedules_data = require("./tb_schedules.json");
+const tb_situation_data = require("./tb_situation.json");
+const tb_unit_data = require("./tb_unit.json");
+const tb_unit_schedules_data = require("./tb_unit_shedules.json");
 
 async function main() {
+  tb_address.forEach(
+    async (address) => await prisma.tb_address.create({ data: address })
+  );
 
-  tb_address.forEach( async (address) => await prisma.tb_address.create({ data: address }))
+  tb_schedules.forEach(
+    async (schedule) => await prisma.tb_schedules.create({ data: schedule })
+  );
 
-  tb_schedules.forEach( async (schedule) => await prisma.tb_schedules.create({ data: schedule }) )
+  tb_situation.forEach(
+    async (situation) => await prisma.tb_situation.create({ data: situation })
+  );
 
-  tb_situation.forEach( async (situation) => await prisma.tb_situation.create( { data: situation } ))
+  tb_unit.forEach(async (unit) => {
+    await prisma.tb_unit.create({ data: unit });
+  });
 
-  tb_unit.forEach( async (unit) => { await prisma.tb_unit.create({ data: unit })})
-
-  tb_unit_schedules.forEach( async (unit_schedule) => await prisma.tb_unit_schedules.create({ data: unit_schedule}))
-  
-
+  tb_unit_schedules.forEach(
+    async (unit_schedule) =>
+      await prisma.tb_unit_schedules.create({ data: unit_schedule })
+  );
 }
 
 main()
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
-const port = process.env.PORT || 3000
+import { Prisma, PrismaClient } from "@prisma/client";
+import express from "express";
+const port = process.env.PORT || 3000;
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/units', async (req, res) => {
- 
+app.get("/units", async (req, res) => {
   const units = await prisma.tb_unit.findMany({
-  
     include: {
       tb_address: true,
       tb_situation: true,
       tb_schedules: {
         include: {
           tb_schedules: true,
-        }
+        },
       },
-      
-    }
-  })
+    },
+  });
 
-
-  res.json(units)
-})
+  res.json(units);
+});
 
 const server = app.listen(port, () =>
   console.log(`
-🚀 Server ready at: http://localhost:${port}`),
-)
+🚀 Server ready at: http://localhost:${port}`)
+);

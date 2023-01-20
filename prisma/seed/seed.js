51a54c172e294156f7f2be03a739f8482b1d8fdb
LuizFelipePinho <luizfelipesilva458@gmail.com>
2023-01-20 15:39:26 -0300

    refect variables data

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -9,23 +9,23 @@ const tb_unit_data = require("./tb_unit.json");
 const tb_unit_schedules_data = require("./tb_unit_shedules.json");
 
 async function main() {
-  tb_address.forEach(
+  tb_address_data.forEach(
     async (address) => await prisma.tb_address.create({ data: address })
   );
 
-  tb_schedules.forEach(
+  tb_schedules_data.forEach(
     async (schedule) => await prisma.tb_schedules.create({ data: schedule })
   );
 
-  tb_situation.forEach(
+  tb_situation_data.forEach(
     async (situation) => await prisma.tb_situation.create({ data: situation })
   );
 
-  tb_unit.forEach(async (unit) => {
+  tb_unit_data.forEach(async (unit) => {
     await prisma.tb_unit.create({ data: unit });
   });
 
-  tb_unit_schedules.forEach(
+  tb_unit_schedules_data.forEach(
     async (unit_schedule) =>
       await prisma.tb_unit_schedules.create({ data: unit_schedule })
   );
